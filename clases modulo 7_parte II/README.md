c8bddf5051ec3052f82c6f7565c296783655788e
Heidi Francia <heidi.cerritos.privados94@clases.edu.sv>
2026-07-22 17:27:39 -0600

    Add files via upload

---
+++ clases modulo 7_parte II/README.md
@@ -0,0 +1,27 @@
+# Proyecto Clases - Séptimo Módulo
+
+Este proyecto agrupa las prácticas desarrolladas en el módulo 7.
+
+## 🚀 Descripción del Proyecto
+
+Este repositorio tiene como objetivo consolidar los conocimientos adquiridos en el módulo: Sistema de Gestión del Colegio San Marcos.
+
+### 🛠️ Tecnologías Utilizadas
+
+* **Lenguaje:** jsx, css, html
+* **Frameworks/Librerías:** react, vite, axios
+* **Otras herramientas:** git
+
+---
+
+## 📋 Características Principales
+
+* Crear, editar alumnos: nombre, apellidos, grado y sección. 
+* Envio de mensajes
+* Visualizar si los alumnos están activos o no
+
+
+---
+
+## 👥 Autora
+Heidi Francia
---
+++ clases modulo 7_parte II/src/App.jsx
@@ -0,0 +1,57 @@
+import { Routes, Route } from 'react-router-dom';
+import { PaginaListaAlumnos } from './pages/PaginaListaAlumnos/PaginaListaAlumnos';
+import { PaginaDetalleAlumno } from './pages/PaginaDetalleAlumno/PaginaDetalleAlumno';
+import { PaginaCrearAlumno } from './pages/PaginaCrearAlumno/PaginaCrearAlumno';
+import { PaginaEditarAlumno } from './pages/PaginaEditarAlumno/PaginaEditarAlumno';
+import { PaginaLogin } from './pages/PaginaLogin/PaginaLogin';
+import { RutaProtegida } from './components/RutaProtegida/RutaProtegida';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/login' element={<PaginaLogin />} />
+
+        <Route
+          path='/'
+          element={
+            <RutaProtegida>
+              <PaginaListaAlumnos />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path='/alumnos/nuevo'
+          element={
+            <RutaProtegida>
+              <PaginaCrearAlumno />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path='/alumnos/:id'
+          element={
+            <RutaProtegida>
+              <PaginaDetalleAlumno />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path='/alumnos/:id/editar'
+          element={
+            <RutaProtegida>
+              <PaginaEditarAlumno />
+            </RutaProtegida>
+          }
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
